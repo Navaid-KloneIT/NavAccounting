b86a143dc1afe436af3a210b00f920e07506f0fe
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-03-01 04:30:34 +0500

    Update DB_ENGINE to use custom MySQL backend

--- navaccounting/settings/development.py
+++ navaccounting/settings/development.py
@@ -4,8 +4,15 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',  # noqa: F405
+        'ENGINE': env('DB_ENGINE', default='navaccounting.db_backends.mysql'),  # noqa: F405
+        'NAME': env('DB_NAME', default='navaccounting'),  # noqa: F405
+        'USER': env('DB_USER', default='root'),  # noqa: F405
+        'PASSWORD': env('DB_PASSWORD', default=''),  # noqa: F405
+        'HOST': env('DB_HOST', default='localhost'),  # noqa: F405
+        'PORT': env('DB_PORT', default='3306'),  # noqa: F405
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
