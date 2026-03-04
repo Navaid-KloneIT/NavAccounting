bfff13e764d235085b163f23dfeb48f7d0f96925
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-03-04 05:39:56 +0500

    Add AR AppConfig with signal import in ready()

---
+++ apps/accounts_receivable/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class AccountsReceivableConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.accounts_receivable'
+    verbose_name = 'Accounts Receivable'
+
+    def ready(self):
+        import apps.accounts_receivable.signals  # noqa: F401
