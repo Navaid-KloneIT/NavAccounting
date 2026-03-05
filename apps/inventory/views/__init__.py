5825537c9684169bdf989acd97695c4dbffb0fdb
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-03-06 02:20:27 +0500

    Add inventory views package init

---
+++ apps/inventory/views/__init__.py
@@ -0,0 +1,7 @@
+from .item_master import *  # noqa: F401, F403
+from .purchasing import *  # noqa: F401, F403
+from .transactions import *  # noqa: F401, F403
+from .costing import *  # noqa: F401, F403
+from .planning import *  # noqa: F401, F403
+from .cycle_count import *  # noqa: F401, F403
+from .landed_cost import *  # noqa: F401, F403
