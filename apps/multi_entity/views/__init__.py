c5c91b87e5f0116c163d5eced20b6291ff6ef4a8
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-03-11 00:12:33 +0500

    Add views package init re-exporting all 6 view submodules

---
+++ apps/multi_entity/views/__init__.py
@@ -0,0 +1,6 @@
+from .entities import *  # noqa: F401, F403
+from .intercompany import *  # noqa: F401, F403
+from .translation import *  # noqa: F401, F403
+from .consolidation import *  # noqa: F401, F403
+from .transfer_pricing import *  # noqa: F401, F403
+from .regulatory import *  # noqa: F401, F403
