2b01dbc06ef8df1b1274e2f9a01c58291bc138e2
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-03-16 00:56:51 +0500

    Add reporting views package init

---
+++ apps/reporting/views/__init__.py
@@ -0,0 +1,10 @@
+from . import (  # noqa: F401
+    financial_statements,
+    management_reports,
+    report_builder,
+    scheduled_reports,
+    xbrl_filing,
+    statutory_reporting,
+    consolidation_reports,
+    dashboards,
+)
