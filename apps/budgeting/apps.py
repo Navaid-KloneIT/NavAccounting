4739048612e7845fcd262df0a37e15d5f527ecde
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-03-17 02:29:20 +0500

    Add budgeting app config with audit trail signals

---
+++ apps/budgeting/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class BudgetingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.budgeting'
+    verbose_name = 'Budgeting & Planning'
+
+    def ready(self):
+        import apps.budgeting.signals  # noqa: F401
